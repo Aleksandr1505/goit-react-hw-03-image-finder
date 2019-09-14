68c387cc6da2d4e74293692ed4a158ef68162847
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-15 00:05:00 +0300

    Updates

---
+++ precache-manifest.443789baef5e5a250bd6d7d86ffaedc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0f792c7b3a90aa1e08be7a9f9865c2",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c08fa0b095393235a81b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.ed848db0.chunk.css"
+  },
+  {
+    "revision": "cb458ce9276d397cd093",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a47679be.chunk.js"
+  },
+  {
+    "revision": "c08fa0b095393235a81b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.8cbf1214.chunk.js"
+  },
+  {
+    "revision": "64eb9730415c0607730d",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.303cb74d.js"
+  }
+]);
